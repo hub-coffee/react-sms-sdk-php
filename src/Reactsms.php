fc1d401655d5f048a19c009f78421516080f8f5c
Huberson Kouakou <huberson.kouakou@merakytech.com>
2024-08-21 10:03:54 +0000

    fix bug

--- src/Reactsms.php
+++ src/Reactsms.php
@@ -23,7 +23,7 @@ class Reactsms {
         return base64_encode($this->authKey.":".$this->apiKey);
     }
 
-    public function request($method, $uri, $options = []) {
+    private function request($method, $uri, $options = []) {
         $options['headers']['Authorization'] = 'Bearer '.$this->build_token();
         $response = $this->httpClient->request($method, $uri, $options);
         return $response->getBody(); //json_decode($response->getBody(), true);
@@ -39,6 +39,16 @@ class Reactsms {
         return $new_phones;
     }
 
+    /**
+     * Send message function
+     * 
+     * @param string $message this parameter contains the message body
+     * 
+     * @param array $phones this parameter contains the phones numbers
+     * 
+     * @return json $data return type is JSON data
+     * 
+     */
     public function send_message(string $message, array $phones) {
         $payload = [
             "message" => $message, 
@@ -49,10 +59,28 @@ class Reactsms {
         return $this->request('POST', "/messages/send", ['json' => $payload]);
     }
 
+    /**
+     * Balance function
+     * 
+     * @return json $data return type is JSON data
+     */
     public function balance() {
         return $this->request('GET', "/messages/get_balance");
     }
 
+    /**
+     * Create service function
+     * 
+     * @param string $service_name
+     * 
+     * @param int $quota_sms
+     * 
+     * @param bool $active_quota
+     * 
+     * @param string $description
+     *     
+     * @return json $data return type is JSON data
+     */
     public function create_service(string $service_name, int $quota_sms, bool $active_quota, string $description) {
         $payload = [
             "service_name" => $service_name,
